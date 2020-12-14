71604cb761d6eb136d49575439a475b12a7c0589
sjaehn <kontakt@jahnichen.de>
2020-12-14 20:46:24 +0100

    Store .path = "" instead of nullptr

--- src/Sample.hpp
+++ src/Sample.hpp
@@ -54,11 +54,10 @@ struct Sample
                 if (!samplepath) return;
 
         	int len = strlen (samplepath);
-                if (!len) return;
-
                 path = (char*) malloc (len + 1);
                 if (!path) throw std::bad_alloc();
                 memcpy (path, samplepath, len + 1);
+                if (!len) return;
 
                 // Extract file extension
                 char* extptr = strrchr (path, '.');
